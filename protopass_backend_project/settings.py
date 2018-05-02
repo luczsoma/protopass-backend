6500980b9a7c11dcea11690a4c0deb956fbe8ab0
bereadam <bere.adam15@gmail.com>
2018-05-02 13:30:03 +0200

    email creds to environment vars

--- protopass_backend_project/settings.py
+++ protopass_backend_project/settings.py
@@ -130,8 +130,8 @@ STATIC_URL = '/static/'
 
 ATOMIC_REQUESTS = True
 
-EMAIL_USE_TLS = email_creds.EMAIL_USE_TLS
-EMAIL_HOST = email_creds.EMAIL_HOST
-EMAIL_HOST_USER = email_creds.EMAIL_HOST_USER
-EMAIL_HOST_PASSWORD = email_creds.EMAIL_HOST_PASSWORD
-EMAIL_PORT = email_creds.EMAIL_PORT
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', True)
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = os.environ.get('EMAIL_PORT', 587)
